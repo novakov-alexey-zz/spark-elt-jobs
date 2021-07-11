1c393e57d5c9f5b27225ad2bcaab2727e0f2c8fc
Alexey Novakov <novakov.alex@gmail.com>
2021-07-11 16:15:24 +0200

    minor refactoring

--- modules/lambda/src/main/scala/etljobs/handler/FileToFileLambda.scala
+++ modules/lambda/src/main/scala/etljobs/handler/FileToFileLambda.scala
@@ -7,12 +7,14 @@ import io.circe.parser.decode
 import io.circe.generic.auto._
 import io.circe.Decoder
 import com.amazonaws.services.lambda.runtime.Context
+import mainargs.Flag
 
 import java.io.{InputStream, OutputStream}
 import java.nio.charset.StandardCharsets.UTF_8
 import java.net.URI
+
 import scala.util.Try
-import mainargs.Flag
+import scala.io.Source
 
 class FileToFileLambda {
 
@@ -30,13 +32,14 @@ class FileToFileLambda {
       out: OutputStream,
       context: Context
   ): Unit = {
-    val payload = scala.io.Source.fromInputStream(in).mkString("")
+    val payload = Source.fromInputStream(in).mkString("")
 
     println(s"Event recieved: $payload")
     println(s"Req id: ${context.getAwsRequestId()}")
     val cfg = decode[FileCopyCfg](payload)
     cfg match {
-      case Left(value) => throw value.fillInStackTrace()
+      case Left(value) =>
+        throw value.fillInStackTrace()
       case Right(copyCfg) =>
         FileToFile.hadoopCopy(copyCfg)
         out.write("OK".getBytes(UTF_8))
